fe2ff757b0f0271e0db5002d46618d909511bccc
Stanislav Atanasov <lucifer@anavaro.com>
2014-05-23 17:15:49 +0300

    Zebra Enhance
    - Language vars

--- zebraenhance/language/en/zebra_enchance.php
+++ zebraenhance/language/en/zebra_enchance.php
@@ -31,11 +31,21 @@ $lang = array_merge($lang, array(
 	
 	'UCP_ZEBRA_ENCHANCE_CONFIRM_CANCEL_ASK'	=>	'Are you sure you want to cancel the friend request?',
 	'UCP_ZEBRA_ENCHANCE_CONFIRM_CANCEL'	=> 'Friend request was cancelled!',
-	
 		
 	'NOTIFICATION_TYPE_ZEBRA_ADD'	=>	'New friend request notification',
 	'NOTIFICATION_ZEBRA_ADD'	=>	'%1$s sent you friend request!',
 	
 	'NOTIFICATION_TYPE_ZEBRA_CONFIRM'	=>	'Confirmation for friend request',
 	'NOTIFICATION_ZEBRA_CONFIRM'	=>	'%1$s confirmed your friend request!',
+	
+	'FRINEDLIST_TITLE'	=>	'Friendlist',
+	
+	'NOT_ENEMY'	=>	'All except foes',
+	'SPECIAL_FRIENDS'	=>	'Special friends',
+	
+	'ZE_FRIENDLIST'	=>	'Friendlist',
+	'ZE_FRIENDLIST_EXPLAIN'	=>	'Who can see your frindlist?',
+	
+	'FRIENDLIST_ERROR_ACCESS'	=>	'You do not have access to see user\'s friendlist.',
+
 ));
